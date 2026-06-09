ad69454dddf9f77722155d5f1fbccb26941ee8ad
Tanya Rath <emailtanya8@gmail.com>
2026-06-09 13:33:40 -0700

    header done

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,23 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+function Header() {
+  return (
+    <Box>
+      <AppBar color="inherit" 
+        sx={{background: "inherit", boxShadow: "none", borderBottom: "1px solid #e5e5e5",}}>
+        <Toolbar sx={{display: "flex", justifyContent: "center"}}>
+          <Typography 
+            variant="h6" 
+            component="div"
+            sx={{fontFamily: "inherit", fontWeight: "inherit", fontSize:  30}}>Wordle</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
